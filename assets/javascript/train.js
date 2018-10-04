09cb6b0d774f8c66cd4a10b1752ba4f3f50f3209
Gonzalon <40928276+Gonzalon3140@users.noreply.github.com>
2018-10-03 18:26:11 -0700

    completed the html and some basic css started on the logic of the game

---
+++ assets/javascript/train.js
@@ -0,0 +1,14 @@
+var index = 0;
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDgnV0Mo5jo0-d5YAF7a0rLC1GKnDFYtj4",
+    authDomain: "trainproject-330c9.firebaseapp.com",
+    databaseURL: "https://trainproject-330c9.firebaseio.com",
+    projectId: "trainproject-330c9",
+    storageBucket: "trainproject-330c9.appspot.com",
+    messagingSenderId: "1091909701701"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
