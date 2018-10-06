5f7c3092ece52ceb41ed2bc03a608cf25be16d2c
Gonzalon <40928276+Gonzalon3140@users.noreply.github.com>
2018-10-06 10:06:48 -0700

    logic is working data is posting need to fix the css

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -12,3 +12,55 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
+console.log(database)
+$("#formID").on("submit", function (event) {
+    event.preventDefault();
+
+    var name = $("#trainName").val().trim();
+    var destination = $("#trainDestination").val().trim();
+    var firstTime = $("#firstTrainTime").val().trim();
+    var frequency = $("#frequency").val().trim();
+
+    database.ref().push({
+        name: name,
+        destination: destination,
+        firstTime: firstTime,
+        frequency: frequency
+    });
+
+    $("#trainName").val("");
+    $("#trainDestination").val("");
+    $("#firstTrainTime").val("");
+    $("#frequency").val("");
+
+    return false;
+});
+
+database.ref().on("child_added", function (snapshot) {
+    let train = snapshot.val()
+
+    var timeArr = train.firstTime.split(":");
+    var trainTime = moment().hours(timeArr[0]).minutes(timeArr[1]);
+    var maxMoment = moment.max(moment(), trainTime);
+    var trainArrival;
+    var trainMinutes;
+
+    if (maxMoment === trainTime) {
+        trainArrival = trainTime.format("hh:mm A");
+        trainMinutes = trainTime.diff(moment(), "minutes");
+    } else {
+
+        // Calculate the minutes until arrival using hardcore math
+        // To calculate the minutes till arrival, take the current time in unix subtract the FirstTrain time
+        // and find the modulus between the difference and the frequency.
+        var differenceTimes = moment().diff(trainTime, "minutes");
+        var tRemainder = differenceTimes % train.frequency;
+        trainMinutes = train.frequency - tRemainder;
+        // To calculate the arrival time, add the tMinutes to the current time
+        trainArrival = moment().add(trainMinutes, "m").format("hh:mm A");
+    }
+    console.log(trainArrival, trainMinutes % 60)
+
+    $("#train-table > tbody").append("<tr><td>" + train.name + "</td><td>" + train.destination + "</td><td>" +
+        train.frequency + "</td><td>" + train.firstTime + "</td><td>" + trainMinutes % 60 + "</td></tr>");
+});
